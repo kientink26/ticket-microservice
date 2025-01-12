c441096071b3ddbface157e9d6543d5fda61e051
kienpt <kientink26@gmail.com>
2025-01-12 19:41:25 +0700

    Refactor update ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -24,33 +24,44 @@ router.put(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const ticket = await Ticket.findById(req.params.id);
+    let ticket = await Ticket.findById(req.params.id);
 
     if (!ticket) {
       throw new NotFoundError();
-    }
-
-    if (ticket.orderId) {
-      throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
 
-    ticket.set({
-      title: req.body.title,
-      price: req.body.price,
-      version: ticket.version + 1,
-    });
-    await ticket.save();
-    new TicketUpdatedPublisher(natsWrapper.client).publish({
-      id: ticket.id,
-      title: ticket.title,
-      price: ticket.price,
-      userId: ticket.userId,
-      version: ticket.version,
-    });
+    ticket = await Ticket.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        orderId: undefined,
+      },
+      {
+        title: req.body.title,
+        price: req.body.price,
+        $inc: {
+          version: 1,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (ticket != null) {
+      new TicketUpdatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId,
+        version: ticket.version,
+      });
+    } else {
+      throw new BadRequestError("Cannot edit a reserved ticket");
+    }
 
     res.send(ticket);
   }
